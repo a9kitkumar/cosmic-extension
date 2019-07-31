0b8196222d543fc241b275aa04aa458691ec42c4
a9kitkumar <a9kitsharma.com>
2019-08-01 01:34:31 +0530

    up

---
+++ build/precache-manifest.3b81871ff38178dfd0082ded0d4a64e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f35fffc0c93709c0ba229c451a54dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2fa34a6b4d79fb91c6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7eef1f57fec18cf31818",
+    "url": "/static/css/main.5be77a3a.chunk.css"
+  },
+  {
+    "revision": "0a2fa34a6b4d79fb91c6",
+    "url": "/static/js/2.17ab4a47.chunk.js"
+  },
+  {
+    "revision": "7eef1f57fec18cf31818",
+    "url": "/static/js/main.a3d044a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
