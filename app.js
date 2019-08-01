2bc662cbb36f7ad0efeabe1f72ea2ffc7dc5bba1
a9kitkumar <46286692+a9kitkumar@users.noreply.github.com>
2019-08-01 20:27:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-module.exports = (req, res) => {
-  res.end(`Hi!`)
-}
+// module.exports = (req, res) => {
+//   res.end(`Hi!`)
+// }
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('./build'));
+
+app.listen(process.env.PORT function () {
+});
