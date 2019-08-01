177f356eb20252b25d36cb932ebd378bf2a4f907
a9kitkumar <46286692+a9kitkumar@users.noreply.github.com>
2019-08-01 21:31:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ var connect = require('connect');
 var serveStatic = require('serve-static'); 
 var app = connect(); 
 
-app.use(serveStatic('./build')); 
+app.use(serveStatic('./dist')); 
 
 app.listen();
